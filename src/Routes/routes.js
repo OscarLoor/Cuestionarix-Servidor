8d6f830a195d6a864d54850bb10cfbafc9bc0535
Oscar Loor <oscar.loor@epn.edu.ec>
2018-12-20 03:46:22 -0500

    Cambio el tipo de metodo para poder enviar parametros usando angular 6 como cliente y permito el acceso a Allow Headers

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,8 +5,9 @@ var routesCreadas = function(app){
     app.route('/ingresar')
         .post(UsuarioController.authUsuario); //Envio email y contraseña
     
+    app.route('/mostrarUsuarios')
+        .post(UsuarioController.mostrarUsuarios) //Envio token y id de usuario, es POST para poder enviar token y el id
     app.route('/usuarios')
-        .get(UsuarioController.mostrarUsuarios) //Envio token y id de usuario
         .post(UsuarioController.crearUsuario) //Envio datos de usuario
         .put(UsuarioController.modificarUsuario) //Envio datos de usuario
         .delete(UsuarioController.eliminarUsuario); //Envio el id del usuario a eliminar
